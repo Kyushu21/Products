20f844156f9c1b02ef380bd69ab9d534d201bb60
Kyushu21 <owen_play@hotmail.com>
2023-05-21 14:08:01 -0700

    Nuevo form

---
+++ Products/Interfaz.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Products
+{
+    partial class Interfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Barra = new System.Windows.Forms.Panel();
+            this.MenuVertical = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Barra
+            // 
+            this.Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.Barra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Barra.Location = new System.Drawing.Point(0, 0);
+            this.Barra.Name = "Barra";
+            this.Barra.Size = new System.Drawing.Size(1300, 35);
+            this.Barra.TabIndex = 0;
+            // 
+            // MenuVertical
+            // 
+            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuVertical.Location = new System.Drawing.Point(0, 35);
+            this.MenuVertical.Name = "MenuVertical";
+            this.MenuVertical.Size = new System.Drawing.Size(200, 615);
+            this.MenuVertical.TabIndex = 1;
+            // 
+            // Interfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.Controls.Add(this.MenuVertical);
+            this.Controls.Add(this.Barra);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Interfaz";
+            this.Text = "Interfaz";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Barra;
+        private System.Windows.Forms.Panel MenuVertical;
+    }
+}
